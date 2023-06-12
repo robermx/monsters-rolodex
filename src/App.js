2ec8ccfbbbd3566f32a9fc551558f7abb9632acd
Roberto Arenas <roberto.arenas2@icloud.com>
2023-06-12 13:13:29 -0600

    [Update] Flittering list by monsters name

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-
 import './App.css';
 
 class App extends Component {
@@ -7,13 +6,43 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: '',
     };
   }
+
+  componentDidMount() {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    fetch(url)
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => ({ monsters: users }),
+          () => console.log(this.state)
+        )
+      );
+  }
+
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+    console.log(filteredMonsters);
     return (
       <div className="App">
+        <input
+          type="search"
+          className="search-box"
+          placeholder="Search monsters"
+          onChange={(e) => {
+            const searchField = e.target.value.toLowerCase();
+
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
         <ul>
-          {this.state.monsters.map((monster) => (
+          {filteredMonsters.map((monster) => (
             <li key={monster.id}>{monster.name}</li>
           ))}
         </ul>
